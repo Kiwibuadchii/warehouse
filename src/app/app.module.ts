b83ced1f1916c72dc1462726e1d31f71b1a05bf1
sarayut <sarayutj.tong@gmail.com>
2024-09-15 15:37:25 +0700

    16-9-24

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
+import { AddItemComponent } from './add-item/add-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    ItemDetailsComponent,
+    AddItemComponent
   ],
   imports: [
     BrowserModule,
